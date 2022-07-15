457b2725e4f0ef6c47a9c09c020af2a09b2fd5df
Adolfo Neto <adolfo.usp@gmail.com>
2022-07-15 13:50:04 -0300

    I ran mix format now

--- test/projetinho_live_web/views/error_view_test.exs
+++ test/projetinho_live_web/views/error_view_test.exs
@@ -9,6 +9,7 @@ defmodule ProjetinhoLiveWeb.ErrorViewTest do
   end
 
   test "renders 500.html" do
-    assert render_to_string(ProjetinhoLiveWeb.ErrorView, "500.html", []) == "Internal Server Error"
+    assert render_to_string(ProjetinhoLiveWeb.ErrorView, "500.html", []) ==
+             "Internal Server Error"
   end
 end
